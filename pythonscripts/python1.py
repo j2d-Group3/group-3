0d833d0d2d5da3fedafcccb5e46fc0fbf4545ff4
vanitha <vinnivanitha99@gmail.com>
2017-12-24 15:24:26 +0530

    python scripts

---
+++ pythonscripts/python1.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+
+
+# This program adds two numbers
+
+num1 = 1
+num2 = 6
+
+# Add two numbers
+num3 = (num1) + (num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, num3))
